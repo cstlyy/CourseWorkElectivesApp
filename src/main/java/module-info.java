b5b9fc8bc432e0dba2ff6beec798d95f6c23d521
cstlyy <ohnocringen@gmail.com>
2025-10-06 22:03:50 +0300

    pom edit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module com.example.courseworkappelectives {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.base;
+    requires java.sql;
+    requires java.desktop;
 
     opens com.example.courseworkappelectives to javafx.fxml;
     exports com.example.courseworkappelectives;
