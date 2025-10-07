02004a681d9862c9e0028a02988428937b393206
cstlyy <ohnocringen@gmail.com>
2025-10-07 11:42:42 +0300

    login add

--- src/main/java/com/example/courseworkappelectives/LoginController.java
+++ src/main/java/com/example/courseworkappelectives/LoginController.java
@@ -3,7 +3,7 @@ package com.example.courseworkappelectives;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class LoginController {
     @FXML
     private Label welcomeText;
 
